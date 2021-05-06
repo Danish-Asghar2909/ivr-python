9c33072d3f3010ee05a81f414a37115ff40c523e
Danish-Asghar2909 <danish.2909@icloud.com>
2021-05-06 14:09:20 +0530

    adding select functionality after call test one

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def hello():
 def welcome():
       response = VoiceResponse()
       with response.gather(
-        num_digits=1, voice="alice", language="en-IN", action=url_for('welcome',_scheme='http',_external=True), method="POST"
+        num_digits=1, voice="alice", language="en-IN", action=url_for('welcomeCB',_scheme='http',_external=True), method="POST"
       ) as g:
         g.say(message="Thanks for calling Vibconnect. IVR Phone Home Service. " +
               " Please press 1 for Table reservation." +
@@ -27,12 +27,113 @@ def welcome():
         # g.say(message="please press the correct number")
       return vibconnect(response)
 
+
+
 @app.route("/test")
 def test():
     response = VoiceResponse()
     response.dial("917596035307")
     return vibconnect(response)
 
+@app.route('/welcomeCB', methods=['POST'])
+def welcomeCB():
+    app.logger.error("DTMFGathertime response = %s" % request.get_json())
+    digit = None
+    if request.get_json() is not None:
+        if 'Digits' in request.get_json():
+            selected_option = request.json.get('Digits')
+    #selected_option = request.form['Digits']
+    app.logger.error("welcomeCB digit received = %s" % selected_option)
+    option_actions = {'1': "tablebooking",
+                      '2': "loyalitypoint",
+                      '3': "otherquery"}
+
+    if selected_option in option_actions:
+        if int(selected_option) == 1:
+            response = _tablereservation()
+            return vibconnect(response)
+        elif int(selected_option) == 2:
+            response = _loyality_point(request.json.get('From'))
+            return vibconnect(response)
+        elif int(selected_option) == 3:
+            response = _forotherquery()
+            return vibconnect(response)
+    else:
+        return _redirect_welcome()
+
+def _tablereservation():
+    response = VoiceResponse()
+    with response.gather(
+        numDigits=1, action=url_for('reservation_day',_scheme='http',_external=True), method="POST"
+    ) as g:
+        g.say("Press 1 for today " +
+              "Press 2 for tomorrow " +
+              "To go back to the main menu " +
+              " press the star key.",
+              voice="alice", language="en-GB", loop=3)
+
+    return response
+
+
+def _loyality_point(customer_number):
+    response = VoiceResponse()
+    req_body = {'customer_key': customer_key, 'merchant_id': merchant_id, 'customer_mobile': customer_number}
+    my_headers = {'x-api-key' : x_api_key,'Content-Type':'application/json','Accept':'application/json'}
+    custresp = requests.get(customer_check_url,headers=my_headers,json=req_body)
+    if (response.status_code == 200):
+        response.say("Your loyality points are "+response.json().response.details.currentpoints,voice="alice", language="en-GB")
+        # Code here will only run if the request is successful
+    else:
+        response.say("To get to _loyality_point, please visit our website",voice="alice", language="en-IN")
+    response.hangup()
+    return response
+
+
+def _tableservationtime_today():
+    response = VoiceResponse()
+    with response.gather(
+        numDigits=1, action=url_for('tableservationtimetoday',_scheme='http',_external=True), method="POST"
+    ) as g:
+        g.say("Press 1 for breakfast, press 2 for lunch, press 3 for dinner" ,voice="alice", language="en-IN", loop=3)
+    return response
+
+def _tableservationtime_tomorrow():
+    response = VoiceResponse()
+    with response.gather(
+        numDigits=1, action=url_for('tableservationtimetomorrow',_scheme='http',_external=True), method="POST"
+    ) as g:
+        g.say("Press 1 for breakfast, press 2 for lunch, press 3 for dinner" ,voice="alice", language="en-IN", loop=3)
+    return response
+
+
+def _forotherquery():
+    response = VoiceResponse()
+    response.dial(reception_number,timeout=30,action=url_for('receptionCB',_scheme='http',_external=True), method="POST")
+    return response
+
+
+
+@app.route('/receptionCB', methods=['GET','POST'])
+def receptionCB():
+    response = VoiceResponse()
+    response.dial(manager_number, timeout=30,action=url_for('managerCB',_scheme='http',_external=True), method="POST")
+    # return response
+    return vibconnect(response)
+
+
+
+@app.route('/managerCB', methods=['GET','POST'])
+def managerCB():
+    response = VoiceResponse()
+    response.dial(owner_number, timeout=30,action=url_for('ownerCB',_scheme='http',_external=True), method="POST")
+    return vibconnect(response)
+
+@app.route('/ownerCB', methods=['GET','POST'])
+def ownerCB():
+    response = VoiceResponse()
+    # response.dial(owner_number, timeout=30,action=url_for('ownerCB',_scheme='http',_external=True), method="POST")
+    response.say(message="thanks for calling owner")
+    return vibconnect(response)
 
     
 
---
+++ config.py
@@ -0,0 +1,22 @@
+customer_check_url = "https://hwxr8k5ubg.execute-api.ap-south-1.amazonaws.com/customercheck/merchant/posCustomerCheck"
+
+x_api_key = "9rL7h6DutV3XKgxOSLQba7XbzWGHg6Py2bjyNFdf"
+
+reception_number = "918240182045"
+manager_number = "919903333376"
+owner_number = "917596035307"
+demo_number="917596035307"
+
+sender_id = "TINIYO"
+
+auth_id = "TINIYO_AUTH_ID"
+auth_secret = "TINIYO_AUTH_SECRET_ID"
+
+customer_key = "15657test"
+merchant_id = 15657
+
+domain_prefix = "https://tiniyo.dev"
+
+# For sending SMS
+tiniyo_authid = ""
+tiniyo_authsecretid = ""
